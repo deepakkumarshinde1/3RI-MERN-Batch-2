cb4f6d97281f3b365daeb7c3a7ed37f44a713559
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-03-28 09:11:54 +0530

    ES-6 Day-2

--- Advance JS/js/es6.js
+++ Advance JS/js/es6.js
@@ -1,27 +1,32 @@
-// destructuring --> React , Node JS
-// extracting data from array or jsObject
-let array = [10, 20, 30, 40, 50];
+// oop
+// alert();
 
-// let numberOne = array[0];
-// let numberTwo = array[1];
-// let numberThree = array[2];
+/// core logic of alert is been overridden
+// js don't have methods over loading
 
-let [numberOne, numberTwo] = array;
-console.log(numberOne, numberTwo);
+// class a blue print object
+// class is collection props & methods
+// props => variable
+// method ==> function
+// this ==> current instance of a class
+class PopUp {
+  text = "Welcome to 3ri technology";
+  alert() {
+    setTimeout(() => {
+      console.log(this);
+    }, 1000);
+  }
+}
+// use class methods and property we need object of class
+// PopUp() => constructors
+let popUp = new PopUp();
+popUp.alert();
 
-let product = {
-  name: "Mi Note 11",
-  brand: "MI",
-  prices: 12000,
-  stock: 10,
-  locality: {
-    city: "pune",
-  },
-};
+//run a code after 1 sec === 1000 ms
 
-let { city } = product.locality;
-let { name: pName, stock, prices, locality } = product;
+// this ==> window ==> Window
 
-console.log(pName);
-console.log(stock);
-console.log(prices);
+// global variables & function are attached to window object
+
+// when we use "function" key word function, scope of "this" will be current function call object
+// when we use "=>" i.e arrow function, scope of "this" will be nearest function definition object
