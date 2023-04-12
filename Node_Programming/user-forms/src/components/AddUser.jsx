4f6423f415924a2d79a24ceaca8ec32add07c15e
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-04-12 08:31:14 +0530

    Forms

--- Node_Programming/user-forms/src/components/AddUser.jsx
+++ Node_Programming/user-forms/src/components/AddUser.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
 const AddUser = () => {
+  let initialDetails = { fullName: "", mobile: "", gender: "", city: "" };
   let [userList, setUserList] = useState([]);
-  let [newUser, setNewUser] = useState({
-    fullName: "",
-  }); // string number boolean array object
+  let [newUser, setNewUser] = useState({ ...initialDetails }); // string number boolean array object
 
   let inputChange = (event) => {
     // get value of input => event.target.value
-    let { value } = event.target;
-    console.log("changed", value);
+
+    // primitive & non-primitive
+    let { value, name } = event.target;
+    console.log(name);
+    // let _newUser = { ...newUser };
+    // _newUser[name] = value;
+    // setNewUser(_newUser);
+    setNewUser({ ...newUser, [name]: value });
   };
   let saveUser = (event) => {
     event.preventDefault();
     console.log("submit click");
+
+    // let _userList = [...userList];
+    // _userList.push({ ...newUser });
+    // setUserList(_userList);
+
+    setUserList([...userList, { ...newUser }]);
+    setNewUser({ ...initialDetails });
   };
   return (
     <>
@@ -28,8 +40,68 @@ const AddUser = () => {
               type="text"
               placeholder="Enter Full Name"
               className="form-control"
+              name="fullName"
+              value={newUser.fullName}
               onChange={inputChange}
             />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="" className="form-label">
+              Mobile
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Mobile Number"
+              className="form-control"
+              name="mobile"
+              value={newUser.mobile}
+              onChange={inputChange}
+            />
+          </div>
+          <div className="d-flex mb-2">
+            <label htmlFor="">Gender</label>
+            <div className="form-check ms-2">
+              <input
+                className="form-check-input"
+                type="radio"
+                value="Male"
+                id="flexCheckDefault"
+                name="gender"
+                checked={newUser.gender === "Male" ? true : false}
+                onChange={inputChange}
+              />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Male
+              </label>
+            </div>
+            <div className="form-check ms-2">
+              <input
+                className="form-check-input"
+                type="radio"
+                value="Female"
+                id="flexCheckChecked"
+                name="gender"
+                onChange={inputChange}
+                checked={newUser.gender === "Female" ? true : false}
+              />
+              <label className="form-check-label" htmlFor="flexCheckChecked">
+                Female
+              </label>
+            </div>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">City</label>
+            <select
+              value={newUser.city}
+              className="form-select"
+              name="city"
+              onChange={inputChange}
+            >
+              <option value="">Select</option>
+              <option value="Pune">Pune</option>
+              <option value="Mumbai">Mumbai</option>
+              <option value="Nashik">Nashik</option>
+            </select>
           </div>
           <div className="d-flex gap-4 justify-content-center">
             <button className="btn btn-success w-50">Save</button>
